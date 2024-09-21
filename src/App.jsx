e663594a31ea8072ecf40815a858bd78a8ffd5ce
Jefino9488 <jefinojacob9488@gmail.com>
2024-09-22 02:44:05 +0530

    Initial web

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,107 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef } from 'react';
+import './App.css';
+import { Octokit } from 'https://esm.sh/@octokit/core';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [url, setUrl] = useState('');
+  const [region, setRegion] = useState('Global');
+  const [corePatch, setCorePatch] = useState('false');
+
+  const formRef = useRef(null);
+  const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
+  const REPO_OWNER = 'Jefino9488';
+  const REPO_NAME = 'HyperMod-Builder';
+  const HYPER_BUILDER = 'Hyper_Builder.yml';
+
+  const octokit = new Octokit({
+    auth: GITHUB_TOKEN,
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const inputs = { URL: url, region, core: corePatch };
+
+    try {
+      const response = await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
+        owner: REPO_OWNER,
+        repo: REPO_NAME,
+        workflow_id: HYPER_BUILDER,
+        ref: 'builder',
+        inputs: inputs,
+      });
+
+      if (response.status === 204) {
+        window.alert('HyperMod build started! Please wait for 10-15 minutes and check the releases page.');
+        resetForm();
+      } else {
+        console.error('Error triggering GitHub Action:', response.status);
+      }
+    } catch (error) {
+      console.error('Error triggering GitHub Action:', error);
+    }
+  };
+
+  const resetForm = () => {
+    setUrl('');
+    setRegion('Global');
+    setCorePatch('false');
+  };
+
+  const handleRedirect = () => {
+    window.open('https://github.com/Jefino9488/XAGA-builder/releases', '_blank');
+  };
+
+  const handleRedirectBuild = () => {
+    window.open(`https://github.com/Jefino9488/XAGA-builder/actions/workflows/${HYPER_BUILDER}`, '_blank');
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div id="root">
+        <h1 id="h">HyperMod ROM Build</h1>
+        <form ref={formRef} onSubmit={handleSubmit}>
+          <h2>Hyper Mod ROM Details</h2>
+          <label htmlFor="url-input">Recovery ROM direct link:</label>
+          <input
+              type="url"
+              id="url-input"
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+              required
+          />
 
-export default App
+          <label htmlFor="region-select">Select Region:</label>
+          <select
+              id="region-select"
+              value={region}
+              onChange={(e) => setRegion(e.target.value)}
+              required
+          >
+            <option value="CN">CN</option>
+            <option value="Global">Global</option>
+          </select>
+
+          <label htmlFor="core-select">Apply Core Patch:</label>
+          <select
+              id="core-select"
+              value={corePatch}
+              onChange={(e) => setCorePatch(e.target.value)}
+              required
+          >
+            <option value="false">No</option>
+            <option value="true">Yes</option>
+          </select>
+
+          <button type="submit">Start Build</button>
+        </form>
+
+        <p>All builds are available on the releases page</p>
+        <div id="root1">
+          <button onClick={handleRedirect}>Go to releases</button>
+          <button onClick={handleRedirectBuild}>Build Status</button>
+        </div>
+      </div>
+  );
+};
+
+export default App;

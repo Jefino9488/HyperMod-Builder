b97434669fac06e038093bfdbb1b3467c7ec2570
jefino9488 <jefinojacob9488@gmail.com>
2024-10-24 11:33:29 +0530

    test size fix

--- Builder/scripts/repack.sh
+++ Builder/scripts/repack.sh
@@ -18,23 +18,57 @@ sudo chmod +x "${WORKSPACE}/tools/make_ext4fs"
 sudo chmod +x "${WORKSPACE}/tools/mke2fs"
 sudo chmod +x "${WORKSPACE}/tools/resize2fs"
 sudo chmod +x "${WORKSPACE}/tools/simg2img"
+sudo chmod +x "${WORKSPACE}/tools/tune2fs"
 
 echo -e "${YELLOW}- Repacking images"
 if [ "$EXT4" = true ]; then
+    img_free() {
+        size_free="$(sudo tune2fs -l "$WORKSPACE/${DEVICE}/images/${i}.img" | awk '/Free blocks:/ { print $3 }')"
+        size_free="$(echo "$size_free / 4096 * 1024 * 1024" | bc)"
+        if [[ $size_free -ge 1073741824 ]]; then
+          File_Type=$(awk "BEGIN{print $size_free/1073741824}")G
+        elif [[ $size_free -ge 1048576 ]]; then
+          File_Type=$(awk "BEGIN{print $size_free/1048576}")MB
+        elif [[ $size_free -ge 1024 ]]; then
+          File_Type=$(awk "BEGIN{print $size_free/1024}")kb
+        elif [[ $size_free -le 1024 ]]; then
+          File_Type=${size_free}b
+        fi
+        echo -e "\e[1;33m - ${i}.img 剩余空间: $File_Type \e[0m"
+    }
+    for i in product system system_ext vendor; do
+        size_orig=$(sudo du -sb "$WORKSPACE/${DEVICE}/images/$i" | awk '{print $1}')
+
+        # Adjust size based on thresholds and calculate a padded size
+        if [[ "$size_orig" -lt "104857600" ]]; then
+            size=$(echo "$size_orig * 15 / 10 / 4096 * 4096" | bc)
+        elif [[ "$size_orig" -lt "1073741824" ]]; then
+            size=$(echo "$size_orig * 108 / 100 / 4096 * 4096" | bc)
+        else
+            size=$(echo "$size_orig * 103 / 100 / 4096 * 4096" | bc)
+        fi
+
+        # Store the calculated size for each partition
+        eval "$i"_size=$size
+    done
+
     for i in product system system_ext vendor; do
         mkdir -p "$WORKSPACE/${DEVICE}/images/$i/lost+found"
         sudo touch -t 202101010000 "$WORKSPACE/${DEVICE}/images/$i/lost+found"
     done
 
     for partition in product system system_ext vendor; do
+        # Patch fs_config and file_contexts
         sudo python3 "$WORKSPACE/tools/fspatch.py" "$WORKSPACE/${DEVICE}/images/$partition" "$WORKSPACE/${DEVICE}/images/config/${partition}_fs_config"
         sudo python3 "$WORKSPACE/tools/contextpatch.py" "$WORKSPACE/${DEVICE}/images/$partition" "$WORKSPACE/${DEVICE}/images/config/${partition}_file_contexts"
 
         partition_inode=$(sudo wc -l < "$WORKSPACE/${DEVICE}/images/config/${partition}_fs_config")
         partition_inode=$(echo "$partition_inode + 8" | bc)
-        partition_size=$(du -sb "$WORKSPACE/${DEVICE}/images/$partition" | awk '{print $1}')
-        echo -e "${BLUE}- Partition [$partition] size: $partition_size bytes"
-        sudo "$WORKSPACE/tools/make_ext4fs" -s -l "$partition_size" -b 4096 -i "$partition_inode" -I 256 -L "$partition" -a "$partition" -C "$WORKSPACE/${DEVICE}/images/config/${partition}_fs_config" -S "$WORKSPACE/${DEVICE}/images/config/${partition}_file_contexts" "$WORKSPACE/${DEVICE}/images/$partition.img" "$WORKSPACE/${DEVICE}/images/$partition"
+
+        sudo "$WORKSPACE/tools/make_ext4fs" -s -l "$(eval echo \$${partition}_size)" -b 4096 -i "$partition_inode" -I 256 -L "$partition" -a "$partition" -C "$WORKSPACE/${DEVICE}/images/config/${partition}_fs_config" -S "$WORKSPACE/${DEVICE}/images/config/${partition}_file_contexts" "$WORKSPACE/${DEVICE}/images/$partition.img" "$WORKSPACE/${DEVICE}/images/$partition"
+        sudo "$WORKSPACE/tools/resize2fs" -f -M "$WORKSPACE/${DEVICE}/images/$partition.img"
+        eval "$i"_size=$(du -sb "$WORKSPACE"/${DEVICE}/images/$partition.img | awk {'print $partition'})
+        img_free
     done
 else
     for partition in product system system_ext vendor; do
@@ -56,19 +90,22 @@ echo -e "${GREEN}- All partitions repacked"
 move_images_and_calculate_sizes() {
     mkdir -p "${WORKSPACE}/super_maker"
     echo -e "${YELLOW}- Moving images to super_maker and calculating sizes"
+
     local IMAGE
     for IMAGE in vendor product system system_ext odm_dlkm odm vendor_dlkm mi_ext; do
         if [ -f "${WORKSPACE}/${DEVICE}/images/$IMAGE.img" ]; then
             mv -t "${WORKSPACE}/super_maker" "${WORKSPACE}/${DEVICE}/images/$IMAGE.img" || exit
+
+            # Use the previously calculated sizes instead of recalculating
             eval "${IMAGE}_size=\$(du -b \"${WORKSPACE}/super_maker/$IMAGE.img\" | awk '{print \$1}')"
             echo -e "${BLUE}- Moved $IMAGE"
         fi
     done
 
+    # Calculate total size of all images using previously calculated sizes
     echo -e "${YELLOW}- Calculating total size of all images"
-    total_size=$(( ${system_size:-0} + ${system_ext_size:-0} + ${product_size:-0} + ${vendor_size:-0} + ${odm_size:-0} + ${odm_dlkm_size:-0} + ${vendor_dlkm_size:-0} + ${mi_ext_size:-0} ))
-    block_size=4096
-    super_size=$(( (total_size + block_size - 1) / block_size * block_size ))
+    super_size=9126805504
+    total_size=$((${system_size:-0} + ${system_ext_size:-0} + ${product_size:-0} + ${vendor_size:-0} + ${odm_size:-0} + ${odm_dlkm_size:-0} + ${vendor_dlkm_size:-0} + ${mi_ext_size:-0}))
 
     echo -e "${BLUE}- Size of all images"
     echo -e "system: ${system_size:-0}"
@@ -86,11 +123,15 @@ move_images_and_calculate_sizes() {
 create_super_image() {
     echo -e "${YELLOW}- Creating super image"
 
-    lpargs="--metadata-size 65536 --super-name super --block-size 4096 --metadata-slots 3 --device super:${super_size} --group main_a:${super_size} --group main_b:${super_size}"
+    total_size=$(( ${system_size:-0} + ${system_ext_size:-0} + ${product_size:-0} + ${vendor_size:-0} + ${odm_size:-0} + ${odm_dlkm_size:-0} + ${vendor_dlkm_size:-0} + ${mi_ext_size:-0} ))
+    block_size=4096
+    super_size=$(( (total_size + block_size - 1) / block_size * block_size ))
+
+    lpargs="--metadata-size 65536 --super-name super --block-size $block_size --metadata-slots 3 --device super:${super_size} --group main_a:${super_size} --group main_b:${super_size}"
 
     for pname in system system_ext product vendor odm_dlkm odm vendor_dlkm mi_ext; do
         if [ -f "${WORKSPACE}/super_maker/${pname}.img" ]; then
-            subsize=$(du -sb "${WORKSPACE}/super_maker/${pname}.img" | tr -cd 0-9)
+            eval subsize="\$${pname}_size"
             echo -e "${GREEN}Super sub-partition [$pname] size: [$subsize]"
             lpargs="$lpargs --partition ${pname}_a:readonly:${subsize}:main_a --image ${pname}_a=${WORKSPACE}/super_maker/${pname}.img --partition ${pname}_b:readonly:0:main_b"
         fi

f6a8ca3d7e420a1d4a52f4c8faa1b26ded7a9caa
jefino9488 <jefinojacob9488@gmail.com>
2024-10-25 21:41:55 +0530

    remove v

--- Builder/scripts/repack.sh
+++ Builder/scripts/repack.sh
@@ -31,7 +31,7 @@ if [ "$EXT4" = true ]; then
         fs_config="$WORKSPACE/${DEVICE}/images/${i}_fs_config"
         file_contexts="$WORKSPACE/${DEVICE}/images/${i}_file_contexts"
 
-        sudo "$WORKSPACE/tools/e2fsdroid" -v -T 0 -S "$file_contexts" -C "$fs_config" -a "/$i" -s "$WORKSPACE/${DEVICE}/images/$i.img" "$WORKSPACE/${DEVICE}/images/$i"
+        sudo "$WORKSPACE/tools/e2fsdroid" -T 0 -S "$file_contexts" -C "$fs_config" -a "/$i" -s "$WORKSPACE/${DEVICE}/images/$i.img" "$WORKSPACE/${DEVICE}/images/$i"
 
         if [ $? -eq 0 ]; then
             echo "$i image created successfully with e2fsdroid."

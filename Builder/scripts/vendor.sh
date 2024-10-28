ab9b9d4233aebf399097361ff58673a68d28b77c
jefino9488 <jefinojacob9488@gmail.com>
2024-10-28 21:10:36 +0530

    edit fstab for rw

--- Builder/scripts/vendor.sh
+++ Builder/scripts/vendor.sh
@@ -1,5 +1,6 @@
 DEVICE="$1"
 WORKSPACE="$2"
+EXT4=${3:-false}
 
 RED='\033[1;31m'
 YELLOW='\033[1;33m'
@@ -20,6 +21,17 @@ for dir in "${dirs[@]}"; do
     fi
   done
 done
+
+if [[ "$EXT4" == true ]]; then
+  if [ -f "${WORKSPACE}/${DEVICE}/images/vendor/etc/fstab.default" ]; then
+    sudo sed -i "/overlay/d" "${WORKSPACE}/${DEVICE}/images/vendor/etc/fstab.default"
+    cat "${WORKSPACE}/${DEVICE}/images/vendor/etc/fstab.default"
+  fi
+  if [ -f "${WORKSPACE}/${DEVICE}/images/vendor/etc/fstab.emmc" ]; then
+    sudo sed -i "/overlay/d" "${WORKSPACE}/${DEVICE}/images/vendor/etc/fstab.emmc"
+    cat "${WORKSPACE}/${DEVICE}/images/vendor/etc/fstab.emmc"
+  fi
+fi
 
 ls -alh "${WORKSPACE}/${DEVICE}/images/vendor/etc/"
 ls -alh "${WORKSPACE}/${DEVICE}/images/vendor/app/"
